27430f2fae700a69a371347b1d7fe40cd5818d2c
sumin <tnals545@naver.com>
2022-09-20 20:20:31 +0900

    #5.0 Get My Own Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,7 +7,7 @@ const Home = ({ userObj }) => {
   const fileInput = useRef();
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
-  const [attachment, setAttachment] = useState();
+  const [attachment, setAttachment] = useState("");
   useEffect(() => {
     dbService.collection("nweets").onSnapshot((snapshot) => {
       const nweetArray = snapshot.docs.map((doc) => ({
